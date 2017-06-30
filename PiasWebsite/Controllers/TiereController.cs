c3248926f67943ea5dc6fc6722875e826c8892ab
M01POOL16 <M01POOL16@DESKTOP-R1QVA38>
2017-06-30 10:52:24 +0200

    Tiere speichern funktioniert

--- PiasWebsite/Controllers/TiereController.cs
+++ PiasWebsite/Controllers/TiereController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PiasWebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,12 @@ namespace PiasWebsite.Controllers
 		{
 			return View();
 
+		}
+		[HttpPost]
+		public ActionResult Create(Tier tier)
+		{
+			Zoo.Tiere.Add(tier);
+			return RedirectToAction("Index");
 		}
     }
 }
